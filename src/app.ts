8a92975885834d9c66e9ed32921eb9a4cec25ad4
Juan Ramirez <jjramirez013@gmail.com>
2024-02-02 23:04:36 -0600

    fin seccion 14

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,5 @@
 import { envs } from "./config/envs";
+import { AppRoutes } from "./presentation/routes";
 import { Server } from "./presentation/server";
 
 
@@ -18,6 +19,7 @@ function main() {
     const server = new Server({
         port: envs.PORT,
         public_path: envs.PUBLIC_PATH,
+        routes: AppRoutes.routes,
     });
 
     server.start();
