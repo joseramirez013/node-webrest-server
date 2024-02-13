a40a9b76f2ce9a71849b98caba332470f6fbee68
Juan Ramirez <jjramirez013@gmail.com>
2024-02-12 21:28:44 -0600

    fin seccioon 17

---
+++ src/domain/errors/custom.error.ts
@@ -0,0 +1,20 @@
+
+
+
+export class CustomError extends Error {
+
+    constructor(
+        public readonly message: string,
+        public readonly statusCode: number = 400
+    ){
+        super(message);
+    }
+
+
+}
+
+
+
+
+
+
